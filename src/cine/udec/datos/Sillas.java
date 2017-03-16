ebee06f9ace3202f712f2084a2c996092db9cf7b
kenny <user@DESKTOP-LJRI4OC>
2017-03-15 22:26:47 -0500

    Interfaz en consola de el cine

---
+++ src/cine/udec/datos/Sillas.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cine.udec.datos;
+
+/**
+ *
+ * @author kenny
+ */
+public class Sillas {
+    public static void Sillas(){
+        
+    }
+}
